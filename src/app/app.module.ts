6ef788f1fe75a3e32d7838429a979c4117aad4fd
Otman Karim <71219931+Ott00@users.noreply.github.com>
2023-12-11 12:43:18 +0100

    added all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BackOfficeComponent } from './components/back-office/back-office.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegistrationComponent,
+    HomeComponent,
+    DetailsComponent,
+    ProfileComponent,
+    BackOfficeComponent
   ],
   imports: [
     BrowserModule
